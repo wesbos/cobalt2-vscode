e866aedc3f0bf1af084ec4ed97388df843932619
Wes Bos <wesbos@gmail.com>
2017-09-12 13:34:37 -0400

    PROGRESS

--- demo/js.js
+++ demo/js.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use("/static", express.static(path.join(__dirname, "public")));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get("/", function(req, res) {
+  res.send("Hello World!");
 });
-
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+app.use(function(err, req, res, next) {
+  res.status(500).send("Something broke!");
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
