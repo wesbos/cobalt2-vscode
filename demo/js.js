7e46c44d9652a8e0a2dee6a4b594b1c6c86cfea0
Wes Bos <wesbos@gmail.com>
2017-09-14 15:34:44 -0400

    Fix highlight colour

--- demo/js.js
+++ demo/js.js
@@ -1,15 +1,16 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 
-app.use("/static", express.static(path.join(__dirname, "public")));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.get("/", function(req, res) {
+app.get('/', (req, res) => {
   res.send("Hello World!");
 });
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
 app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+  console.log('Example app listening on port 3000!');
 });
